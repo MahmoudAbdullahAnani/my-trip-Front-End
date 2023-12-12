860b38735ab96ca78dde216b1b2f492b76e7faad
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-12-12 15:51:09 +0200

    solve error form search

--- src/components/HandleForm.tsx
+++ src/components/HandleForm.tsx
@@ -4,20 +4,35 @@ import {
   destinationSearch,
   textSearch,
 } from "../data/RecoilState/FormHandling";
+import { useState } from "react";
 
 function HandleForm() {
   const [go] = useRecoilState(textSearch);
-    const [ret] = useRecoilState(destinationSearch);
-    // const getTravels = async () => {
-    //     if (go && ret) {
+  const [ret] = useRecoilState(destinationSearch);
+  const [originCodeState, setOriginCodeState] = useState("");
+  const [returnCodeState, setReturnCodeState] = useState("");
+  console.log(originCodeState);
+  console.log(returnCodeState);
 
-    //     }
-    // }
+  const getTravels = async () => {
+    if (go) {
+      const startIndex = go.indexOf("(");
+      const endIndex = go.indexOf(")");
+      const originCode = go.slice(startIndex + 1, endIndex);
+      setOriginCodeState(originCode);
+    }
+    if (ret) {
+      const startIndex = ret.indexOf("(");
+      const endIndex = ret.indexOf(")");
+      const originCode = ret.slice(startIndex + 1, endIndex);
+      setReturnCodeState(originCode);
+    }
+  };
   return (
     <div className={`flex justify-between `}>
       <Form isOrigin={true} />
       <Form isOrigin={false} />
-      <button className={`bg-green-300`} onClick={() => console.log(`go: ${go}, ret: ${ret}`)}>
+      <button className={`bg-green-300`} onClick={() => getTravels()}>
         Show Values
       </button>
     </div>
