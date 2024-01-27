3c1127d15deabddb49a7029351359fe163e0b332
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-01-27 23:20:55 +0200

    handle search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -26,139 +26,245 @@ import { Loder } from "../components/loder/Loder";
 import CardTrip from "../components/Home/Sections/Search/CardTrip";
 import { iconArrowLeftWhite } from "../assets/icons/home";
 
-const exampleData = [
-  {
-    type: "flight-offer",
-    id: "1",
-    source: "GDS",
-    instantTicketingRequired: false,
-    nonHomogeneous: false,
-    oneWay: false,
-    lastTicketingDate: "2024-01-27",
-    lastTicketingDateTime: "2024-01-27",
-    numberOfBookableSeats: 9,
-    itineraries: [
-      {
-        duration: "PT21H50M",
-        segments: [
-          {
-            departure: {
-              iataCode: "MAD",
-              at: "2024-01-27T21:00:00",
-            },
-            arrival: {
-              iataCode: "NCE",
-              at: "2024-01-27T23:30:00",
-            },
-            carrierCode: "6X",
-            number: "7829",
-            aircraft: {
-              code: "744",
-            },
-            operating: {
-              carrierCode: "6X",
-            },
-            duration: "PT2H30M",
-            id: "131",
-            numberOfStops: 0,
-            blacklistedInEU: false,
-          },
-          {
-            departure: {
-              iataCode: "NCE",
-              terminal: "1",
-              at: "2024-01-28T08:30:00",
-            },
-            arrival: {
-              iataCode: "JFK",
-              terminal: "4",
-              at: "2024-01-28T12:50:00",
-            },
-            carrierCode: "6X",
-            number: "7430",
-            aircraft: {
-              code: "744",
-            },
-            operating: {
-              carrierCode: "6X",
-            },
-            duration: "PT10H20M",
-            stops: [
-              {
-                iataCode: "LGW",
-                duration: "PT1H",
-                arrivalAt: "2024-01-28T09:30:00",
-                departureAt: "2024-01-28T10:30:00",
-              },
-            ],
-            id: "132",
-            numberOfStops: 1,
-            blacklistedInEU: false,
-          },
-        ],
-      },
-    ],
-    price: {
-      currency: "EUR",
-      total: "363.50",
-      base: "295.00",
-      fees: [
-        {
-          amount: "0.00",
-          type: "SUPPLIER",
-        },
-        {
-          amount: "0.00",
-          type: "TICKETING",
-        },
-      ],
-      grandTotal: "363.50",
-      additionalServices: [
-        {
-          amount: "1.00",
-          type: "CHECKED_BAGS",
-        },
-      ],
-    },
-    pricingOptions: {
-      fareType: ["PUBLISHED"],
-      includedCheckedBagsOnly: true,
-    },
-    validatingAirlineCodes: ["6X"],
-    travelerPricings: [
-      {
-        travelerId: "1",
-        fareOption: "STANDARD",
-        travelerType: "ADULT",
-        price: {
-          currency: "EUR",
-          total: "363.50",
-          base: "295.00",
-        },
-        fareDetailsBySegment: [
-          {
-            segmentId: "131",
-            cabin: "BUSINESS",
-            fareBasis: "J6XQSMIX",
-            class: "J",
-            includedCheckedBags: {
-              quantity: 1,
-            },
-          },
-          {
-            segmentId: "132",
-            cabin: "BUSINESS",
-            fareBasis: "J6XQSMIX",
-            class: "J",
-            includedCheckedBags: {
-              quantity: 1,
-            },
-          },
-        ],
-      },
-    ],
-  },
-];
+// const exampleData = [
+//   {
+//     type: "flight-offer",
+//     id: "1",
+//     source: "GDS",
+//     instantTicketingRequired: false,
+//     nonHomogeneous: false,
+//     oneWay: false,
+//     lastTicketingDate: "2024-01-27",
+//     lastTicketingDateTime: "2024-01-27",
+//     numberOfBookableSeats: 7,
+//     itineraries: [
+//       {
+//         duration: "PT5H10M",
+//         segments: [
+//           {
+//             departure: {
+//               iataCode: "CAI",
+//               terminal: "2",
+//               at: "2024-01-27T21:00:00",
+//             },
+//             arrival: {
+//               iataCode: "BAH",
+//               at: "2024-01-28T00:50:00",
+//             },
+//             carrierCode: "GF",
+//             number: "80",
+//             aircraft: {
+//               code: "789",
+//             },
+//             operating: {
+//               carrierCode: "GF",
+//             },
+//             duration: "PT2H50M",
+//             id: "10",
+//             numberOfStops: 0,
+//             blacklistedInEU: false,
+//           },
+//           {
+//             departure: {
+//               iataCode: "BAH",
+//               at: "2024-01-28T01:55:00",
+//             },
+//             arrival: {
+//               iataCode: "DXB",
+//               terminal: "1",
+//               at: "2024-01-28T04:10:00",
+//             },
+//             carrierCode: "GF",
+//             number: "500",
+//             aircraft: {
+//               code: "320",
+//             },
+//             operating: {
+//               carrierCode: "GF",
+//             },
+//             duration: "PT1H15M",
+//             id: "11",
+//             numberOfStops: 0,
+//             blacklistedInEU: false,
+//           },
+//         ],
+//       },
+//     ],
+//     price: {
+//       currency: "EUR",
+//       total: "207.01",
+//       base: "61.00",
+//       fees: [
+//         {
+//           amount: "0.00",
+//           type: "SUPPLIER",
+//         },
+//         {
+//           amount: "0.00",
+//           type: "TICKETING",
+//         },
+//       ],
+//       grandTotal: "207.01",
+//     },
+//     pricingOptions: {
+//       fareType: ["PUBLISHED"],
+//       includedCheckedBagsOnly: true,
+//     },
+//     validatingAirlineCodes: ["GF"],
+//     travelerPricings: [
+//       {
+//         travelerId: "1",
+//         fareOption: "STANDARD",
+//         travelerType: "ADULT",
+//         price: {
+//           currency: "EUR",
+//           total: "207.01",
+//           base: "61.00",
+//         },
+//         fareDetailsBySegment: [
+//           {
+//             segmentId: "10",
+//             cabin: "ECONOMY",
+//             fareBasis: "WDLIT4EG",
+//             brandedFare: "ECOLITE",
+//             brandedFareLabel: "ECONOMY LIGHT",
+//             class: "W",
+//             includedCheckedBags: {
+//               quantity: 2,
+//             },
+//             amenities: [
+//               {
+//                 description: "STANDARD SEAT",
+//                 isChargeable: true,
+//                 amenityType: "PRE_RESERVED_SEAT",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "MEALS AND BEVERAGES",
+//                 isChargeable: false,
+//                 amenityType: "MEAL",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "EXTRA LEGROOM SEATS",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "REFUNDABLE TICKET",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "CHANGEABLE TICKET",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "PREFERRED SEATS",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "50 PERCENT MILES EARNED",
+//                 isChargeable: false,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//             ],
+//           },
+//           {
+//             segmentId: "11",
+//             cabin: "ECONOMY",
+//             fareBasis: "WDLIT4EG",
+//             brandedFare: "ECOLITE",
+//             brandedFareLabel: "ECONOMY LIGHT",
+//             class: "W",
+//             includedCheckedBags: {
+//               quantity: 2,
+//             },
+//             amenities: [
+//               {
+//                 description: "STANDARD SEAT",
+//                 isChargeable: true,
+//                 amenityType: "PRE_RESERVED_SEAT",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "MEALS AND BEVERAGES",
+//                 isChargeable: false,
+//                 amenityType: "MEAL",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "EXTRA LEGROOM SEATS",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "REFUNDABLE TICKET",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "CHANGEABLE TICKET",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "PREFERRED SEATS",
+//                 isChargeable: true,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//               {
+//                 description: "50 PERCENT MILES EARNED",
+//                 isChargeable: false,
+//                 amenityType: "BRANDED_FARES",
+//                 amenityProvider: {
+//                   name: "BrandedFare",
+//                 },
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ];
 
 function Search() {
   // Statement Data of trip
@@ -232,30 +338,31 @@ function Search() {
   // console.log("trip==> ", trip);
   const navigator = useNavigate();
   useEffect(() => {
-    // if (!originLocationCode || !destinationLocationCode || !departureDate) {
-    //   return navigator(`/`);
-    // }
-    // getDataSearch();
+    if (!originLocationCode || !destinationLocationCode || !departureDate) {
+      return navigator(`/`);
+    }
+    getDataSearch();
   }, []);
-  // if (loading) {
-  //   return (
-  //     <div
-  //       className={`z-50 h-[100%] w-full absolute top-0 flex justify-center items-center bg-[#283965]`}
-  //     >
-  //       <Loder />
-  //     </div>
-  //   );
-  // }
-  // if (trip.length <= 0) {
-  //   return <div>Not Found</div>;
-  // }
+  if (loading) {
+    return (
+      <div
+        className={`z-50 h-[100%] w-full absolute top-0 flex justify-center items-center bg-[#283965]`}
+      >
+        <Loder />
+      </div>
+    );
+  }
+  if (trip.length <= 0) {
+    return <div>Not Found</div>;
+  }
   return (
     <section className={``}>
       <header
-        className={`pt-[calc(25px+54px)] pb-[44px] relative bg-d[url('/public/assets/mapHeader.png')]`}
+        className={`lg:hidden pt-[calc(25px+54px)] pb-[89px] relative bg-d[url('/public/assets/mapHeader.png')]`}
       >
+        {/* <div className={`absolute bottom-0 w-full z-20`}>{iconHeaderSearch}</div> */}
         <div
-          className={`lg:hidden flex flex-col gap-[20px] px-[16px] relative z-20 text-[#FFF]`}
+          className={` flex flex-col gap-[20px] px-[16px] relative z-20 text-[#FFF]`}
         >
           <button
             onClick={() => navigator("/")}
@@ -286,10 +393,21 @@ function Search() {
           src="/public/assets/mapHeader.png"
           className={`absolute w-full h-full top-0 object-cover z-10`}
         />
+        <img
+          src="/public/assets/headMobile.png"
+          className={`absolute w-full h-[50px] bottom-0  z-10`}
+        />
       </header>
-      {exampleData.map((trip) => {
-        return <CardTrip {...trip} key={`${trip.id}--${Math.random()}`} />;
-      })}
+      <div>
+        {trip.map((trip: { itineraries: [], id: string }) => {
+          return (
+            <CardTrip
+              itineraries={trip.itineraries}
+              key={`${trip.id}--${Math.random()}`}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 }
