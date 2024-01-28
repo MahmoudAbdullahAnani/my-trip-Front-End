7fc446deef27062927bf76e07b66bcf484ccd05e
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-01-28 10:30:46 +0200

    handle cheng

--- src/components/Home/Sections/PopularDestinations/PopularDestinations.tsx
+++ src/components/Home/Sections/PopularDestinations/PopularDestinations.tsx
@@ -1,48 +1,65 @@
 import PopularDestinationsCard from "./PopularDestinationsCard";
+import exampleMainImage from "/public/assets/heroSection.jpeg";
+
+import Slider from "react-slick";
 
 const data = [
   {
     id: 1,
-    mainImage: "/public/assets/heroSection.jpeg",
+    mainImage: exampleMainImage,
     titleAR: "القاهرة",
     titleEN: "Egypt",
     link: "/",
   },
   {
     id: 2,
-    mainImage: "/public/assets/heroSection.jpeg",
+    mainImage: exampleMainImage,
     titleAR: "القاهرة",
     titleEN: "Egypt",
     link: "/",
   },
   {
     id: 1,
-    mainImage: "/public/assets/heroSection.jpeg",
+    mainImage: exampleMainImage,
     titleAR: "القاهرة",
     titleEN: "Egypt",
     link: "/",
   },
   {
     id: 1,
-    mainImage: "/public/assets/heroSection.jpeg",
+    mainImage: exampleMainImage,
     titleAR: "القاهرة",
     titleEN: "Egypt",
     link: "/",
   },
 ];
+
 function PopularDestinations() {
+    const settings = {
+      className: "center",
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 5,
+      swipeToSlide: true,
+      afterChange: function (index:number) {
+        console.log(
+          `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+        );
+      },
+    };
   return (
-    <section
-      className={`flex flex-col gap-[34px] mt-[107px]`}
-    >
+    <section className={`flex flex-col gap-[34px] mt-[107px] pe-[96px]`}>
       <h2 dir="rtl" className={`text-[#000] text-[24px] font-[700] `}>
         الوجهات الرائجة
       </h2>
-      <div className={`flex overflow-x-scroll gap-[24px]`}>
+      <Slider {...settings}  className={`flex gap-[24px]`}>
         {data.map((data) => (
-          <PopularDestinationsCard {...data} key={`${data.id}----${Math.random()}`}/>
+          <PopularDestinationsCard
+            {...data}
+            key={`${data.id}----${Math.random()}`}
+          />
         ))}
-      </div>
+      </Slider>
     </section>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -22,6 +22,8 @@ import "react-datepicker/dist/react-datepicker.css";
 
   import "react-toastify/dist/ReactToastify.css";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
