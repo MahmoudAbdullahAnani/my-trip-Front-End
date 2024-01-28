c0feccffcb18883e886ac32ec4e2fc9389ece39a
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-01-28 16:02:47 +0200

    add Destination Section

---
+++ src/components/Home/Sections/Destination/DestinationSection.tsx
@@ -0,0 +1,23 @@
+function DestinationSection() {
+  return (
+    <section
+      className={`lg:mt-[337px] mt-[200px] relative flex justify-center items-center h-[531px] bg-no-repeat bg-cover bg-[url('/public/assets/DestinationSection/DestinationSection-1.png')]`}
+    >
+      <div
+        style={{
+          background: "rgba(0, 0, 0, 0.30)",
+        }}
+        className={`w-full h-full absolute z-0`}
+      ></div>
+      <div
+        className={`text-[#FFF] text-center z-10 lg:text-[116.477px] text-[86px] font-[600] GHAITHSANS`}
+      >
+        <h2 className={``}>أجــــواء</h2>
+        <h2 className={``}>AJWAA</h2>
+        <h4 className={`text-[36px]  font-[600]`}>الوجهة الاولى لاي مسافر</h4>
+      </div>
+    </section>
+  );
+}
+
+export default DestinationSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import MoveBgHeroSection from "../components/Home/Systems/MoveBgHeroSection";
 import HeroSection from "../components/Home/Systems/HeroSection";
 import OffersSection from "../components/Home/Sections/Offers/OffersSection";
 import PopularDestinations from "../components/Home/Sections/PopularDestinations/PopularDestinations";
+import DestinationSection from "../components/Home/Sections/Destination/DestinationSection";
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       </section>
       <OffersSection />
       <PopularDestinations />
+      <DestinationSection />
     </>
   );
 }
