342b81d0feb205dbe9463f462e534f6f0383cc33
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-02-15 23:45:23 +0200

    add oauth

--- src/data/RecoilState/AuthStatePages/Auth.tsx
+++ src/data/RecoilState/AuthStatePages/Auth.tsx
@@ -4,15 +4,19 @@ export const openLoginPageState = atom({
   key: "openLoginPageState", // unique ID (with respect to other atoms/selectors)
   default: false, // default value (aka initial value)
 });
-export const openForgotPasswordPageState = atom({
-  key: "openForgotPasswordPageState", // unique ID (with respect to other atoms/selectors)
-  default: false, // default value (aka initial value)
-});
+
 export const openSignupPageState = atom({
   key: "openSignupPageState", // unique ID (with respect to other atoms/selectors)
   default: false, // default value (aka initial value)
 });
+
+// Password
 export const showPassword = atom({
   key: "showPassword", // unique ID (with respect to other atoms/selectors)
   default: true, // default value (aka initial value)
+});
+
+export const openForgotPasswordPageState = atom({
+  key: "openForgotPasswordPageState", // unique ID (with respect to other atoms/selectors)
+  default: false, // default value (aka initial value)
 });
