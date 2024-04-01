de5a201a381086d2c4aeedade42b651d2e2720c1
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-04-01 17:34:48 +0200

    handle admin chat

--- src/components/ChatSingleUser.tsx
+++ src/components/ChatSingleUser.tsx
@@ -49,8 +49,13 @@ function ChatSingleUser({
       message.message.message.SenderNumber === chatSocketId ||
       message.message.userId === userId
   );
-  // console.log(messagesState);
-  // console.log(messageData);
+  // const senderUser = messagesState.filter(
+  //   (message) => message.message.message.SenderNumber === chatSocketId
+  // );
+  // const reciverAdmin = messagesState.filter(
+  //   (message) => message.message.userId === userId
+  // );
+  // console.log(senderUser);
 
   return (
     <>
@@ -87,10 +92,83 @@ function ChatSingleUser({
                   : "start"
               }`}
             >
-              <p>{message.message.message.message}</p>
+              <div
+                className={`flex flex-col gap-1 text-${
+                  message.message.userId === userId ? "start" : "end"
+                } bg-slate-100 my-1 px-2 py-1 rounded-lg`}
+              >
+                <div
+                  className={`flex justify-${
+                    message.message.userId === userId ? "start" : "end"
+                  }  items-center gap-2`}
+                >
+                  {message.message.userId === userId ? (
+                    <>
+                      <img
+                        width={28}
+                        height={28}
+                        src={message.avatar}
+                        className={`rounded-full`}
+                        alt={`${message.firstName}-${message.lastName}`}
+                      />
+                      <h6
+                        className={`text-[10px] `}
+                      >{`${message.firstName} ${message.lastName}`}</h6>
+                    </>
+                  ) : (
+                    <>
+                      <h6
+                        className={`text-[10px] `}
+                      >{`${message.firstName} ${message.lastName}`}</h6>
+                      <img
+                        width={28}
+                        height={28}
+                        src={message.avatar}
+                        className={`rounded-full`}
+                        alt={`${message.firstName}-${message.lastName}`}
+                      />
+                    </>
+                  )}
+                </div>
+                <p>{message.message.message.message}</p>
+              </div>
             </div>
           ))}
         </div>
+        {/* <div>
+          {senderUser.map((message: Message) => (
+            <div
+              key={`--${Math.random()}--${Math.random()}`}
+              className={`text-${
+                arabic_letters.includes(
+                  message.message.message.message === undefined
+                    ? ""
+                    : message.message.message.message[0]
+                )
+                  ? "end"
+                  : "start"
+              }`}
+            >
+              <div
+                className={`flex flex-col text-end bg-slate-100 my-1 px-2 py-1 rounded-lg`}
+              >
+                <div className={`flex justify-end items-center gap-2`}>
+                  <h6
+                    className={`text-[10px] `}
+                  >{`${message.firstName} ${message.lastName}`}</h6>
+                  <img
+                    width={28}
+                    height={28}
+                    src={message.avatar}
+                    className={`rounded-full`}
+                    alt={`${message.firstName}-${message.lastName}`}
+                  />
+                </div>
+                <p>{message.message.message.message}</p>
+              </div>
+            </div>
+          ))}
+        </div> */}
       </div>
     </>
   );
