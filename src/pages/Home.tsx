4988cffffa770bd42563c92f1d069e4cf60745bc
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-04-29 20:39:25 +0300

    remove sectionOffers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import MoveBgHeroSection from "../components/Home/Systems/MoveBgHeroSection";
 import HeroSection from "../components/Home/Systems/HeroSection";
-import OffersSection from "../components/Home/Sections/Offers/OffersSection";
+// import OffersSection from "../components/Home/Sections/Offers/OffersSection";
 import PopularDestinations from "../components/Home/Sections/PopularDestinations/PopularDestinations";
 import DestinationSection from "../components/Home/Sections/Destination/DestinationSection";
 import { useEffect } from "react";
